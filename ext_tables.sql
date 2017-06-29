b642bd69e18157af3ca86964929647eb1ee1683a
Javier Andrade Cobo <javier@backendstudio.com>
2017-06-29 17:06:01 -0500

    Fixes to make extension work in translated pages

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,3 +5,11 @@ CREATE TABLE pages (
 	css_class varchar(255) DEFAULT '' NOT NULL,
 	css_id varchar(255) DEFAULT '' NOT NULL,
 );
+
+#
+# Modifying pages_language_overlay table
+#
+CREATE TABLE pages_language_overlay (
+  css_class varchar(255) DEFAULT '' NOT NULL,
+  css_id varchar(255) DEFAULT '' NOT NULL,
+);
